b22c5bd87026aab8e7c1d6e68c17c6a8f10f9369
Guy Skinner <guy.c.skinner@kcl.ac.uk>
2018-11-29 12:38:37 +0000

    pair clusters cleaned up

--- src/main.cxx
+++ src/main.cxx
@@ -8,83 +8,15 @@
  or http://opensource.org/licenses/mit-license.php for information.
 */
 
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <numeric>
-#include <vector>
-
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/matrix_proxy.hpp>
-#include <boost/numeric/ublas/vector.hpp>
+#include <iostream>
+
+#include "clusters.hxx"
 #include "neighbour.hxx"
 #include "utils.hxx"
 
 namespace ublas = boost::numeric::ublas;
-
-class Clusters {
-
-public:
-
-  Clusters();
-  void FindClusters(const Neighbour& neighbour_table,
-		    ublas::matrix<double> basis);
-
-  std::vector<double> PairClusters;
-  std::vector<long> PairCount;
-  
-private:
-
-};
-
-Clusters::Clusters() {
-}
-
-void Clusters::FindClusters(const Neighbour& neighbour_table,
-			    ublas::matrix<double> basis) {
-
-  /* Loop Over Neighbours to Find, Number and Sort Pairs */
-  ublas::matrix<double> table = neighbour_table.Sites;
-  std::vector<double> pairs = {};
-  std::vector<long> count = {};
-  
-  /* Assume Equivalent Atoms */
-  ublas::matrix_row<ublas::matrix<double>> b(basis,0);
-    
-  for (auto j = 0; j < neighbour_table.Total(0); j++) {
-    auto ptr = neighbour_table.Pointers(j);
-    ublas::matrix_row<ublas::matrix<double>> site(table,ptr);
-    double r = ublas::norm_2(site - b);
-    long index = linear_search(pairs,r,1e-6);
-    if (index == pairs.size()) {
-      pairs.insert(pairs.begin(),r);
-      count.insert(count.begin(),1);
-    } else {
-      count[index]++;
-    }
-  }
-
-  /* Sorting */
-  std::vector<std::size_t> indices(pairs.size());
-  std::iota(indices.begin(),indices.end(),0);
-  std::sort(indices.begin(), indices.end(),
-	    [&pairs](std::size_t left, std::size_t right) {
-	      return pairs[left] < pairs[right];
-	    });
-
-  std::vector<double> sort_pairs = pairs;
-  std::vector<long> sort_count = count;
-  
-  for (auto i = 0; i < pairs.size(); i++) {
-    sort_pairs[i] = pairs[indices[i]];
-    sort_count[i] = count[indices[i]];
-  }
-
-  PairClusters = sort_pairs;
-  PairCount = sort_count;
-  
-}
 
 int main(void) {
 
@@ -129,6 +61,11 @@ int main(void) {
   
   Clusters clusters = Clusters();
   clusters.FindClusters(neighbour_table,basis);
-    
+
+  for (auto i = 0; i < clusters.Number; i++) {
+    std::cout << clusters.PairClusters[i] << ' ';
+    std::cout << clusters.PairCount[i] << '\n';
+  }
+      
   return 0;
 }
